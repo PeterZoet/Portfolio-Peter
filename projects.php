0d215aa1667723d37a450a2154fefc995bb17d67
Peter <101114264+JustPeter64@users.noreply.github.com>
2023-02-03 16:02:43 +0100

    dashboard.php

--- projects.php
+++ projects.php
@@ -7,22 +7,33 @@
     <meta name="description" content="Portfolio of a software developer">
     <meta name="keywords" content="software developer, portfolio, web development">
     <title>Portfolio - projects</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/index.css">
+
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/projects.css">
 </head>
 <body>
     <?php
         include 'includes/header.php';
         include 'includes/dbConnectie.php';
     ?>
-    <main>
+    <main id='main'>
         <h1>Projects</h1>
-        <div>
-        </div>
+            <div>
+                <h2>Project 1</h2>
+                <p>Project 1 description</p>    
+            </div>
+            <div>
+                <h2>Project 2</h2>
+                <p>Project 2 description</p>
+            </div>
+            <div>
+                <h2>Project 3</h2>
+                <p>Project 3 description</p>
+            </div>
     </main>
     
 <?php
